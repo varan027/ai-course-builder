4949c370a7933fe83b1eec3e04cebe98b55f547d
Varan <varanlucky02@gmail.com>
2026-09-13 21:43:59 +0530

    feat: migrate persisted skill lesson fields

---
+++ prisma/migrations/20260913214500_add_skill_lessons/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "GoalSkill" ADD COLUMN "lessonOverview" TEXT;
+ALTER TABLE "GoalSkill" ADD COLUMN "lessonKeyIdeas" TEXT;
+ALTER TABLE "GoalSkill" ADD COLUMN "lessonContent" TEXT;
+ALTER TABLE "GoalSkill" ADD COLUMN "lessonPractice" TEXT;
