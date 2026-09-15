9d98d57f4cce06d049c81d9039ec434d4d60280a
Varan <varanlucky02@gmail.com>
2026-09-15 18:29:33 +0530

    chore: collapse PostgreSQL schema into a single baseline

--- prisma/migrations/20260913214500_add_skill_lessons/migration.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "GoalSkill" ADD COLUMN "lessonOverview" TEXT;
-ALTER TABLE "GoalSkill" ADD COLUMN "lessonKeyIdeas" TEXT;
-ALTER TABLE "GoalSkill" ADD COLUMN "lessonContent" TEXT;
-ALTER TABLE "GoalSkill" ADD COLUMN "lessonPractice" TEXT;
